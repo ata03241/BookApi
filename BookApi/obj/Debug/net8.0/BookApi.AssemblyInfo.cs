d6ce35daabb175f7e90e6a309e94850abfbcaba6
Ashok Tamang <pakhrintamangashok@gmail.com>
2025-06-22 11:44:34 +0200

    downloaded Angular to .net project

--- BookApi/obj/Debug/net8.0/BookApi.AssemblyInfo.cs
+++ BookApi/obj/Debug/net8.0/BookApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81dca986593ea08c309c9d43f23f0138d4dffc35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1135e3756b8f49fe5b74eea5b43099ecd97ae04")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
