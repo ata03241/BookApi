d690213cd571a9451e19738bd80c23a9fe4108f4
Ashok Tamang <pakhrintamangashok@gmail.com>
2025-07-07 21:03:24 +0200

    health

--- BookApi/obj/Debug/net8.0/BookApi.AssemblyInfo.cs
+++ BookApi/obj/Debug/net8.0/BookApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a590c67e8ad1e6ec5b20ee868886fcf3493bc384")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b4262664ea0fba9eca687b8bb9740f6b55359f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
